a05774091c4fe06b17826471ac151e06c5aef309
Simongluck <sglucksmann@udesa.edu.ar>
2026-06-08 15:21:04 -0300

    Register terminado

---
+++ Xstation/src/Screens/Register.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { View, Text, Pressable, TextInput } from "react-native";
+import { auth, db } from "../firebase/config";
+
+function Register({ navigation }) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [registerError, setRegisterError] = useState("")
+
+    function onSubmit() {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((response) => {
+                db.collection("users").add({
+                    email: email,
+                    username: username,
+                })
+                navigation.navigate("Login");
+            })
+            .catch((error) => {
+                setRegisterError(error.message);
+                console.log(error);
+            });
+    }
+
+    return (
+        <View>
+            <Text>Registro</Text>
+
+            <TextInput keyboardType="default" placeholder="username" onChangeText={text => setUsername(text)} value={username} />
+            <TextInput keyboardType="email-address" placeholder="email" onChangeText={text => setEmail(text)} value={email} />
+            <TextInput keyboardType="default" placeholder="password" secureTextEntry={true} onChangeText={text => setPassword(text)} value={password} />
+            {registerError !== "" && <Text>{registerError}</Text>}
+            <Pressable onPress={() => onSubmit()}>
+                <Text>Registrate</Text>
+            </Pressable>
+
+            <Pressable onPress={() => navigation.navigate("Login")}>
+                <Text>Ir al Login</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default Register;
+
