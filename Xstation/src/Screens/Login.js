40367e7f42d8df68a19146864e4c4c331de33798
Simongluck <sglucksmann@udesa.edu.ar>
2026-06-08 15:45:35 -0300

    Login terminado con duda

---
+++ Xstation/src/Screens/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { View, Text, Pressable, TextInput } from "react-native";
+import { auth, db } from "../firebase/config";
+
+function Login({ navigation }) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loginError, setLoginError] = useState("")
+
+    function onSubmit() {
+        auth.signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                navigation.navigate("Home")
+            })
+            .catch((error) => {
+                setLoginError(error.message)
+                console.log(error)
+            })
+    }
+
+    return (
+        <View>
+            <Text>Login</Text>
+
+            <TextInput keyboardType="email-address" placeholder="email" onChangeText={text => setEmail(text)} value={email} />
+            <TextInput keyboardType="default" placeholder="password" secureTextEntry={true} onChangeText={text => setPassword(text)} value={password} />
+            {loginError !== "" && <Text>{loginError}</Text>}
+            <Pressable onPress={() => onSubmit()}>
+                <Text>Iniciar sesión</Text>
+            </Pressable>
+
+            <Pressable onPress={() => navigation.navigate("Register")}>
+                <Text>Ir al Register</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default Login;
