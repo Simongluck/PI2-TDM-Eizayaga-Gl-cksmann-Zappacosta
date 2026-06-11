b7814c9f20ef1b06a9e7c4bfa9a78bc936376c1b
manueleiza <meizayaga@udesa.edu.ar>
2026-06-11 15:44:59 -0300

    Crear Posts hecho (creo)

--- Xstation/src/Navigation/NavigationTabs.js
+++ Xstation/src/Navigation/NavigationTabs.js
@@ -5,7 +5,9 @@ import Profile from "../Screens/Profile";
 import Home from "../Screens/Home";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Entypo from '@expo/vector-icons/Entypo';
+import createPost from "../Screens/CreatePost";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 
 const Tab = createBottomTabNavigator()
@@ -15,6 +17,7 @@ function NavigationTabs() {
 
         <Tab.Navigator>
             <Tab.Screen name="Home" options={{ headerShown: false, tabBarIcon:()=> <Entypo name="home" size={24} color="black" />  }} component={Home}  />
+            <Tab.Screen name= "CreatePost" options={{headerShown: false, tabBarIcon:()=><MaterialIcons name="add-box" size={24} color="black" />}} component={createPost} />
             <Tab.Screen name="Profile" options={{ headerShown: false, tabBarIcon:()=><MaterialCommunityIcons name="face-man-profile" size={24} color="black" /> }} component={Profile} />
         </Tab.Navigator>
 
